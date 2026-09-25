62d03063511db6ebad09bfdd0e4b181eaea1e03b
Kasmir Khan <127107041+kasmirhdm@users.noreply.github.com>
2026-09-26 02:18:00 +0700

    Bump WaliNet PWA cache to v4 to refresh Apps Script endpoint

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="walinet-pwa-v3";
+const CACHE="walinet-pwa-v4";
 const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icons/icon.svg"];
 
 self.addEventListener("install",event=>{
