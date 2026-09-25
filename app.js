fea851f24259eb257cbed3a032da7abd2d7b62e5
Kasmir Khan <127107041+kasmirhdm@users.noreply.github.com>
2026-09-26 01:31:05 +0700

    Update API WaliNet ke deployment Apps Script terbaru

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const CONFIG={APPS_SCRIPT_URL:"https://script.google.com/macros/s/AKfycbyGUETSX_i63epE1ian0ueqWUGw9FUYuDcc8KRe-0WvyCqn10xYYm2VOSVMJLJQ7dj3qA/exec"};
+const CONFIG={APPS_SCRIPT_URL:"https://script.google.com/macros/s/AKfycby3FiFsvekZIytOkSchiB4iDDNCsUJSnIgTUek-uuBr3rev_ZwZlqurVp8PnkuYqDD7Zg/exec"};
 const data={customers:[],payments:[],packages:[]};
 const money=n=>new Intl.NumberFormat("id-ID",{style:"currency",currency:"IDR",maximumFractionDigits:0}).format(Number(n)||0);
 const esc=s=>String(s??"").replace(/[&<>"']/g,m=>({"&":"&amp;","<":"&lt;",">":"&gt;",'"':"&quot;","'":"&#039;"}[m]));
