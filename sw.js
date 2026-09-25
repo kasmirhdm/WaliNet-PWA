07a6921e8dc9d2c82d55abe2ed443ec744e45fa2
Kasmir Khan <127107041+kasmirhdm@users.noreply.github.com>
2026-09-26 02:28:24 +0700

    Bump WaliNet service worker cache to v5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="walinet-pwa-v4";
+const CACHE="walinet-pwa-v5";
 const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icons/icon.svg"];
 
 self.addEventListener("install",event=>{
